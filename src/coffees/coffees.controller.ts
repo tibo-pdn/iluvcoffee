1c8a8a143b72bd5db4bb5142a7242ef055bdd914
Tibo Pendino <tibo.pendino@epitech.eu>
2024-05-09 15:27:17 -0700

    feat(04-43): Diving Deeper Into RequestScoped Providers

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Inject,
   Param,
   Patch,
   Post,
@@ -12,10 +13,17 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeesService: CoffeesService) {}
+  constructor(
+    private readonly coffeesService: CoffeesService,
+    @Inject(REQUEST) private readonly request: Request,
+  ) {
+    console.log('CoffeeController created');
+  }
 
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
