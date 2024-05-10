1539c3edd5bd1173ba96b4d4361b14046c01e258
Tibo Pendino <tibo.pendino@epitech.eu>
2024-05-10 16:20:47 -0700

    [BONUS] feat(09-76): Creating a Mongoose Model

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { CoffeesController } from "./coffees.controller";
-import { CoffeesService } from "./coffees.service";
+import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Coffee.name,
+        schema: CoffeeSchema,
+      },
+    ]),
+  ],
   controllers: [CoffeesController],
-  providers: [CoffeesService]
+  providers: [CoffeesService],
 })
 export class CoffeesModule {}
