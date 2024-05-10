71947f1289ef199c4c738372e0072eaed8bb2504
Tibo Pendino <tibo.pendino@epitech.eu>
2024-05-10 11:17:12 -0700

    feat(07-64): Adding Example Responses

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -17,18 +17,19 @@ import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Public } from '../common/decorators/public.decorator';
 import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
 import { Protocol } from '../common/decorators/protocol.decorator';
+import { ApiForbiddenResponse } from '@nestjs/swagger';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
   @Public()
   @Get()
   async findAll(
     @Protocol('https') protocol: string,
     @Query() paginationQuery: PaginationQueryDto,
   ) {
-    console.log(protocol);
     return this.coffeesService.findAll(paginationQuery);
   }
 
@@ -44,10 +45,7 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto,
-  ) {
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
