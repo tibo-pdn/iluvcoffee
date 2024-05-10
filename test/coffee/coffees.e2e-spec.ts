5ad9f88220c1f97c92c4fe67a5ba9356a1f02a97
Tibo Pendino <tibo.pendino@epitech.eu>
2024-05-10 15:52:05 -0700

    feat(08-71): Implementing e2e Test Logic - FIX TEST ISSUES WITH DATABASE

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,9 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { CreateCoffeeDto } from '../../src/coffees/dto/create-coffee.dto';
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
+
   let app: INestApplication;
 
   beforeAll(async () => {
@@ -24,10 +32,34 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
     await app.init();
   });
 
-  it.todo('Create [POST /]');
+  it('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED);
+    // .then(({ body }) => {
+    //   const expectedCoffee = jasmine.objectContaining({
+    //     ...coffee,
+    //     flavors: jasmine.arrayContaining(
+    //       coffee.flavors.map((name) => jasmine.objectContaining({ name })),
+    //     ),
+    //   });
+    //   expect(body).toEqual(expectedCoffee);
+    // });
+  });
   it.todo('Get all [GET /]');
   it.todo('Get one [GET /:id]');
   it.todo('Update one [PATCH /:id]');
